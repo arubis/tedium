069aee13cd1b46e3cf18549b8f802a2708d94de3
Dylan Fitzgerald <dylan@dylanfitzgerald.net>
2024-11-05 08:34:19 -0700

    chore: update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,32 @@
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require "bundler/setup"
 require "tedium"
-require "tedium/cli"
-require "tedium/runner"
 require "tempfile"
 require "yaml"
 
+Dir[File.expand_path('support/**/*.rb', __dir__)].each { |f| require f }
+
 RSpec.configure do |config|
   config.disable_monkey_patching!
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
+  
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_modules_order = :random
+  config.filter_run_when_matching :focus
+  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.disable_monkey_patching!
+  config.warnings = true
+
+  if config.files_to_run.one?
+    config.default_formatter = "doc"
+  end
+
+  config.order = :random
+  Kernel.srand config.seed
 end

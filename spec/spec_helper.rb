3ec6cb36871b5fdd7d02dbd52c7e1e069331e5a7
Dylan Fitzgerald <dylan@dylanfitzgerald.net>
2024-11-05 08:16:17 -0700

    chore: remove unused requires from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,9 @@
 require "bundler/setup"
 require "tedium"
-require "tedium/cli"
-require "tedium/runner"
 require "tempfile"
 require "yaml"
 
 RSpec.configure do |config|
-  config.example_status_persistence_file_path = ".rspec_status"
   config.disable_monkey_patching!
   config.expect_with :rspec do |c|
     c.syntax = :expect
